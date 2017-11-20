963518f8b3f2470e7646525949656462f4bfbfb4
artlenk.ru@gmail.com <df5ukoi7ht>
2017-11-20 22:45:00 +0300

    Created the visible part of the site header

--- src/js/common.js
+++ src/js/common.js
@@ -13,93 +13,29 @@ $(document).ready(function () {
         document.head.appendChild(msViewportStyle)
     }
 
-    $('.home-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        fade: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000
-    });
 
-    $('.demonstrations-slider').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        fade: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 3000
-    });
+    const headerSearch = $('.header-search'),
+        navWrap = $('.nav-wrap'),
+        headerSearchNav = 'header-search-nav',
+        headerSearchOpened = 'header-search-opened',
+        navWrapOpened = 'nav-wrap-opened';
 
-// Определяем мобильный браузер
-    function MobileDetect() {
-        var UA = navigator.userAgent.toLowerCase();
-        return (/android|webos|iris|bolt|mobile|iphone|ipad|ipod|iemobile|blackberry|windows phone|opera mobi|opera mini/i.test(UA)) ? true : false;
-    }
-
-    var articleMeta = $(".article-meta"),
-        articleMetaFixed = 'fixed';
-
-    $window = $(window);
-
-// Если браузер не мобильный, работаем
-    if (!MobileDetect()) {
-        if (articleMeta.length) {
-            // Определяем координаты верха блока навигации
-            $h = articleMeta.offset().top;
-            $window.scroll(function () {
-                // Если прокрутили скролл ниже макушки блока, включаем фиксацию
-                if ($window.scrollTop() > $h) {
-                    articleMeta.addClass(articleMetaFixed);
-                } else {
-                    //Иначе возвращаем всё назад
-                    articleMeta.removeClass(articleMetaFixed);
-                }
-            });
-        }
-    }
-
-
-    $('.count-minus').click(function () {
-        var $input = $(this).parent().find('input');
-        var count = parseInt($input.val()) - 1;
-        count = count < 1 ? 1 : count;
-        $input.val(count);
-        $input.change();
-        return false;
-    });
-    $('.count-plus').click(function () {
-        var $input = $(this).parent().find('input');
-        $input.val(parseInt($input.val()) + 1);
-        $input.change();
-        return false;
-    });
-
-    // BEGIN
-    $(".order-next").click(
+    $('.header-nav-toggle').click(
         function () {
-            var basketStepsActive = "basket-steps__active",
-                hidden = 'hidden';
-            $('#basket-step-1').removeClass(basketStepsActive);
-            $('#basket-step-2').addClass(basketStepsActive);
-            $('#msCart').addClass(hidden);
-            $('.ms2_form').removeClass(hidden);
-        }
-    );
-    // END
+            navWrap.toggleClass(navWrapOpened);
+            headerSearch.toggleClass(headerSearchOpened).toggleClass(headerSearchNav);
+        });
 
-    var catListItemOpened = 'catalog-list__item-opened',
-        catListItemEffect = 'catalog-list__item-effect';
 
-    $('.catalog-list__item').hover(
+    $('.icon-search').click(
         function () {
-            $(this).addClass(catListItemOpened).delay(600).addClass(catListItemEffect);
-        },
+            headerSearch.toggleClass(headerSearchOpened);
+        });
+
+    $('.header-cancel').click(
         function () {
-            $(this).removeClass(catListItemEffect).delay(600).removeClass(catListItemOpened);
+            headerSearch.removeClass(headerSearchOpened);
+            navWrap.removeClass(navWrapOpened);
+            headerSearch.removeClass(headerSearchNav);
         });
 });
